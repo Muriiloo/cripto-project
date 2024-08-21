59be1035881ada5d1d7c58ae8b359fafa80c7fe4
Murilo <murilooliveira110305@gmail.com>
2024-08-20 23:43:34 -0300

    feat: add value to input and useNavigate

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,34 @@
-import styles from "./home.module.css"
-import { BsSearch } from "react-icons/bs"
-import { Link } from "react-router-dom"
+import styles from "./home.module.css";
+import { BsSearch } from "react-icons/bs";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, FormEvent } from "react";
 
-export function Home(){
+export function Home() {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e: FormEvent){
+    e.preventDefault(); // não vai recarregar a página
+
+    if(input === '')return;
+
+    navigate(`/detail/${input}`);
+
+    
+  }
 
   return (
     <main className={styles.container}>
-      <form className={styles.form}>
-        <input type="text" placeholder="Digite o nome da moeda"/>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input type="text" 
+        placeholder="Digite o nome da moeda"
+        value={input}
+        onChange={(e)=> setInput(e.target.value)}
+        />
         <button type="submit">
-          <BsSearch size={30} color="#FFF"/>
+          <BsSearch size={30} color="#FFF" />
         </button>
       </form>
-
-
 
       <table>
         <thead>
@@ -36,29 +51,24 @@ export function Home(){
               </div>
             </td>
 
-          <td className={styles.tdLabel} data-label="Valor do Mercado">
-            1T
-          </td>
+            <td className={styles.tdLabel} data-label="Valor do Mercado">
+              1T
+            </td>
 
-          <td className={styles.tdLabel} data-label="Preço">
-            8.00
-          </td>
+            <td className={styles.tdLabel} data-label="Preço">
+              8.00
+            </td>
 
-          <td className={styles.tdLabel} data-label="Volume">
-            2B
-          </td>
+            <td className={styles.tdLabel} data-label="Volume">
+              2B
+            </td>
 
-          <td className={styles.tdProfit} data-label="Mudança 24h">
-            <span>1.20</span>
-          </td>
-
-
+            <td className={styles.tdProfit} data-label="Mudança 24h">
+              <span>1.20</span>
+            </td>
           </tr>
         </tbody>
-
       </table>
-
     </main>
-  )
-
+  );
 }
